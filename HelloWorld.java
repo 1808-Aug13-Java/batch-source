642cb24dccddad75d430e312a4514dc62db5e1bf
Nozuko Sutherland <nozukosutherland@gmail.com>
2018-08-13 20:56:25 -0400

    This is Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.revature.operators;
+
+/**
+ * @author Nozuko Sutherland-Millie
+ * This class prints Hello World
+ */
+public class HelloWorld {
+	
+	/**
+	 * @param args
+	 */
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Displays output
+    }
+}
