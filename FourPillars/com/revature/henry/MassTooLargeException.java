f9b02dafbf8fbd8052e198356728cd09433c6f13
hsaing94 <hsaing94@gmail.com>
2018-08-15 10:03:56 -0400

    fixing some stuff

--- FourPillars/com/revature/henry/MassTooLargeException.java
+++
@@ -1,9 +0,0 @@
-package com.revature.henry;
-
-public class MassTooLargeException extends RuntimeException{
-	
-	    public MassTooLargeException(String message) {
-	        super("Mass cannot be that large");
-	    }
-
-	}
