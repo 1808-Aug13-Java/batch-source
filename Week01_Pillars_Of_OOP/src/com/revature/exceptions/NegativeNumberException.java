a885a4a024289ba3b3adc5668ef85ddb7605e5b9
David Calkins <david.calkins.1000@gmail.com>
2018-08-14 21:31:52 -0400

    Added serial ID to NegativeNumberException

--- Week01_Pillars_Of_OOP/src/com/revature/exceptions/NegativeNumberException.java
+++ Week01_Pillars_Of_OOP/src/com/revature/exceptions/NegativeNumberException.java
@@ -6,18 +6,19 @@ package com.revature.exceptions;
 public class NegativeNumberException extends IllegalArgumentException {
 	
 	
-	/**
-	 * Constructs a new NegativeNumberException with a default message. 
-	 */
+	/** A generated serial ID required by extending the IllegalArgumentException
+	 * class. */
+	private static final long serialVersionUID = -4598696230690765490L;
+
+
+	/** Constructs a new NegativeNumberException with a default message. */
 	public NegativeNumberException() {
 		super("Negative Numbers are not allowed. ");
 	} // end of constructor NegativeNumberException
 	
 	
-	/**
-	 * Constructs a new NegativeNumberException with the specified message. 
-	 * @param message The message of the exception
-	 */
+	/** Constructs a new NegativeNumberException with the specified message. 
+	 * @param message The message of the exception */
 	public NegativeNumberException(String message) {
 		super(message);
 	} // end of constructor NegativeNumberException
