a25fe3c2fb97415d9c3fd44610e2540fef746498
Jan Balangue <jantonio.balangue@gmail.com>
2018-08-17 10:38:39 -0400

    Modify CompareNames.java

--- RandomQuestionApplication/src/com/revature/questionapplication/CompareNames.java
+++ RandomQuestionApplication/src/com/revature/questionapplication/CompareNames.java
@@ -6,15 +6,15 @@ public class CompareNames implements Comparator<Student> {
 
 	@Override
 	public int compare(Student o1, Student o2) {
-		if (o1.getLastName().charAt(0) > o2.getLastName().charAt(0)) {
+		if (o1.getLastName().charAt(0) < o2.getLastName().charAt(0)) {
 			return 1;
 		} else if (o1.getLastName().charAt(0) == o2.getLastName().charAt(0)) {
 			if (o1.getLastName().charAt(1) < o1.getLastName().charAt(1)) {
 				return 1;
 			} else {
-				if (o1.getFirstName().charAt(0) > o2.getFirstName().charAt(0)) {
+				if (o1.getFirstName().charAt(0) < o2.getFirstName().charAt(0)) {
 					return 1;
-				} else if (o1.getFirstName().charAt(1) > o2.getFirstName().charAt(1)) {
+				} else if (o1.getFirstName().charAt(1) < o2.getFirstName().charAt(1)) {
 					return 1;
 				}
 			}
