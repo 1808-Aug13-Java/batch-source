9065fd204bf49feb2fdb4c9343efbe1d8d8e8680
Chandrika Sinha <sinhac@rpi.edu>
2018-08-22 23:34:38 -0400

    Completed 2.2 ORDER BY

--- Chinookcercises.sql
+++ Chinookcercises.sql
@@ -12,3 +12,15 @@ WHERE LASTNAME = 'King';
 SELECT *
 FROM CHINOOK.EMPLOYEE
 WHERE FIRSTNAME = 'Andrew' AND REPORTSTO IS NULL;
+
+-------------------------------------------------
+-- 2.2 ORDER BY
+-------------------------------------------------
+
+SELECT *
+FROM CHINOOK.ALBUM
+ORDER BY TITLE DESC;
+
+SELECT *
+FROM CHINOOK.CUSTOMER
+ORDER BY CITY; -- ascending by default but could also add ASC
