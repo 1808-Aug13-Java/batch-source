3984cd7f6247cb864fbfe82a34a69329582544a3
Newton Hoac <nhoac001@ucr.edu>
2018-08-29 01:32:18 -0400

    Finished JUnit tests for DaoImpl's. All I want to test

---
+++ Project0/src/com/revature/test/BalanceDaoImplTest.java
@@ -0,0 +1,96 @@
+package com.revature.test;
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+
+import com.revature.dao.BalanceDao;
+import com.revature.dao.BalanceDaoImpl;
+import com.revature.dao.LoginDao;
+import com.revature.dao.LoginDaoImpl;
+import com.revature.model.Balance;
+import com.revature.model.Login;
+
+public class BalanceDaoImplTest {
+	BalanceDao bdi = new BalanceDaoImpl();
+	Balance b;
+	LoginDao ldi = new LoginDaoImpl();
+	Login l;
+	
+	// Tests assume Bank.sql is last change to database
+	@Test
+	public void getBalancesTestValid() {
+		List<Balance> listBalances = new ArrayList<>();
+		b = new Balance("user", new BigDecimal("200.03"));
+		listBalances.add(b);
+		b = new Balance("josh.pen", new BigDecimal("15.3"));
+		listBalances.add(b);
+		b = new Balance("user@email.com", new BigDecimal("6000.15"));
+		listBalances.add(b);
+		
+		assertEquals(listBalances, bdi.getBalances());
+	}
+	
+	public void getBalancesTestInvalid() {
+		List<Balance> listBalances = new ArrayList<>();
+		b = new Balance("user", new BigDecimal("2.03"));
+		listBalances.add(b);
+		b = new Balance("josh.pen", new BigDecimal("15.3"));
+		listBalances.add(b);
+		b = new Balance("user@email.com", new BigDecimal("6000.15"));
+		listBalances.add(b);
+		
+		assertNotEquals(listBalances, bdi.getBalances());
+	}
+	
+	@Test
+	public void getBalanceByIdTestValid() {
+		b = new Balance("user", new BigDecimal("200.03"));
+		
+		assertEquals(b, bdi.getBalanceById("user"));
+	}
+	
+	@Test
+	public void getBalanceIdNotValid() {
+		assertEquals(null, bdi.getBalanceById("null"));
+	}
+	
+	@Test
+	public void createBalanceInvalid() {
+		b = new Balance("newuser", new BigDecimal("0"));
+		assertEquals(0, bdi.createBalance(b));
+	}
+	
+	@Test
+	public void createBalanceValid() {
+		l = new Login ("newuser", "pswrd");
+		ldi.createLogin(l);
+		b = new Balance("newuser", new BigDecimal("0"));
+		assertEquals(1, bdi.createBalance(b));
+	}
+	
+	@Test
+	public void deleteBalanceInvalid() {
+		bdi.deleteBalanceById("newuser");
+		assertEquals(0, bdi.deleteBalanceById("newuser"));
+	}
+	
+	@Test
+	public void deleteBalanceValid() {
+		l = new Login ("newuser", "pswrd");
+		ldi.createLogin(l);
+		b = new Balance("newuser", new BigDecimal("0"));
+		assertEquals(0, bdi.deleteBalanceById("newuser"));
+	}
+	
+	@After
+	public void refresh() {
+		ldi.deleteLoginById("newuser");
+		bdi.deleteBalanceById("newuser");
+	}
+
+}
---
+++ Project0/src/com/revature/test/LoginDaoImplTest.java
@@ -0,0 +1,86 @@
+package com.revature.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Test;
+
+import com.revature.dao.LoginDao;
+import com.revature.dao.LoginDaoImpl;
+import com.revature.model.Login;
+
+public class LoginDaoImplTest {
+	LoginDao ldi = new  LoginDaoImpl();
+	Login l;
+	
+	// Tests assume Bank.sql is last change to database
+	@Test
+	public void getLoginsTestValid() {
+		List<Login> listLogins = new ArrayList<>();
+		l = new Login("user", "password");
+		listLogins.add(l);
+		l = new Login("josh.pen", "kappa123");
+		listLogins.add(l);
+		l = new Login("user@email.com", "password");
+		listLogins.add(l);
+		
+		assertEquals(listLogins, ldi.getLogins());
+	}
+	
+	@Test
+	public void getLoginsTestInvalid() {
+		List<Login> listLogins = new ArrayList<>();
+		l = new Login("user", "pp");
+		listLogins.add(l);
+		l = new Login("josh.pen", "kappa123");
+		listLogins.add(l);
+		l = new Login("user@email.com", "password");
+		listLogins.add(l);
+		
+		assertNotEquals(listLogins, ldi.getLogins());
+	}
+	
+	@Test
+	public void getLoginByIdTestValid() {
+		l = new Login("user", "password");
+		
+		assertEquals(l, ldi.getLoginById("user"));
+	}
+	
+	@Test
+	public void getLoginByIdTestInvalid() {
+		assertEquals(null, ldi.getLoginById("null"));
+	}
+	
+	@Test
+	public void createLoginInvalid() {
+		l = new Login("user", "diffpassword");
+		assertEquals(0, ldi.createLogin(l));
+	}
+	
+	@Test
+	public void createLoginValid() {
+		l = new Login("new", "cool");
+		assertEquals(1, ldi.createLogin(l));
+	}
+	
+	@Test
+	public void deleteLoginInvalid() {
+		assertEquals(0, ldi.deleteLoginById("new"));
+	}
+
+	@Test
+	public void deleteLoginValid() {
+		l = new Login("new", "cool");
+		ldi.createLogin(l);
+		assertEquals(1, ldi.deleteLoginById("new"));
+	}
+	
+	@After
+	public void refresh() {
+		ldi.deleteLoginById("new");
+	}
+}
