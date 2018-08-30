8a377ec0dd15dad4acbe2b873443fb2a713f2d13
Carolyn Rehm <carolynhrehm@gmail.com>
2018-08-29 21:42:03 -0400

    week 3 wednesday

---
+++ FrontEndDemos/Day2/ClosureDemo.js
@@ -0,0 +1,21 @@
+// let counter = 0;
+// function add(){
+//     counter += 1;
+//     console.log(counter);
+// }
+
+// add();
+// add();
+// add();
+
+
+
+function add(){
+    let counter = 0;
+    counter += 1;
+    console.log(counter);
+}
+
+add();
+add();
+add();
