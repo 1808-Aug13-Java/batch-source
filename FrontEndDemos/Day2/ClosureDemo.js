cb81fe09a09e10c85db1df8a205b7aa350e41e8d
Carolyn Rehm <carolynhrehm@gmail.com>
2018-08-30 11:52:14 -0400

    Week 3 - Thursday Morning

--- FrontEndDemos/Day2/ClosureDemo.js
+++ FrontEndDemos/Day2/ClosureDemo.js
@@ -10,12 +10,56 @@
 
 
 
+// function add(){
+//     let counter = 0;
+//     counter += 1;
+//     console.log(counter);
+// }
+
+// add();
+// add();
+// add();
+
 function add(){
     let counter = 0;
-    counter += 1;
-    console.log(counter);
+    return function(){
+        return ++counter;
+    }
 }
 
-add();
-add();
-add();
+
+/// callback functions: passing functions as parameters
+function apply2(fun){
+    return fun(2); // return add5(2)
+}
+
+// function add5(param){
+//     return param+5; // return 2+5
+// }
+// let add5 = function (param){
+//     return param+5;
+// }
+
+let add5 = (param)=>{
+    return param+5;
+}
+
+console.log(apply2(add5));
+
+
+/// object creation methods
+let person = {"name": name, "age": age};
+
+//maker function
+function makePerson(name, age){ 
+	let person = {"name": name, "age": age}; 
+    return person;
+}
+let person2 = makePerson("Sally", 30);
+    
+//constructor
+function Person(name, age){
+	this.name = name;
+	this.age = age;
+}
+let person3 = new Person("Molly", 45);
