7ac8e276f6d2b0cfcf2fb3fe04d0565df4905d22
David Calkins <david.calkins.1000@gmail.com>
2018-09-01 19:01:47 -0400

    Finished 4 & 5 Implementation

--- WebDev/Week03/Dom Assignment/JavascriptAssignment.js
+++ WebDev/Week03/Dom Assignment/JavascriptAssignment.js
@@ -24,6 +24,10 @@ function setAnchorLinks() {
 } // end of setAnchorLinks
 
 
+
+
+
+
 // 2. Disable Pluto as a planet of residence. 
 function disablePluto() {
 	// Get the form control with the planet listings
@@ -44,6 +48,10 @@ function disablePluto() {
 	}
 
 } // disablePluto
+
+
+
+
 
 
 // 3. A function that displays the alien text when another planet besides earth
@@ -67,9 +75,22 @@ function alienText() {
 
 
 
+
+
+
+
+
+
 // 4.1. When the submit button is clicked, this function gets all of the input 
 // from the form, and makes a new table row. 
 function onSubmitClick() {
+	// The minimum number of characters for a name
+	const NAME_LENGTH = 2;
+	// The number of numbers in a phone number
+	const P_NUMBER_LENGTH = 10;
+	// The length of a country code. 
+	const P_CONTRY_CODE_LENGTH = 4;
+
 	// Get the first name node
 	let firstNameNode = document.getElementById("firstname");
 
@@ -95,47 +116,173 @@ function onSubmitClick() {
 	let activities = [];
 	for (checkbox of checkboxes) {
 		if (checkbox.checked) {
-			// console.log("Checked: " + checkbox.value);
-			activities.push(checkbox.innerText);
-		}
-		else {
-			// For testing
-			// console.log("Not Checked: " + checkbox.value);
+			// Note: value is the most accurate value here without hard coding. 
+			activities.push(checkbox.value);
 		}
 	}
 
 	// If any of the values are empty, do nothing as the input is invalid
-	//TODO: Possibly implement adding red outline to individual invalid fields. 
 	if (firstNameString == "" || lastNameString == "" || emailString == "" || 
 		phoneString == "" || birthdayString == "" || planetString == "" || 
 		colorString == "" || genderNode == null) 
 	{
-		console.log("Invalid Input");
+		alert("Please fill in emptry fields");
+		return;
+	}
+	// If either of the name fields are empty or have less than two characters, 
+	// alert the user of invalid input and do not add to the table. 
+	if (firstNameString.length < NAME_LENGTH ||
+		lastNameString.length < NAME_LENGTH) 
+	{
+		alert("Names must be at least two characters long. ");
 		return;
 	}
 
+	// If the email is not formatted correctly, alert the user of invalid input 
+	// and do not add to the table. 
+	if (!isValidEmail(emailString)) {
+		alert("The email is not valid. ");
+		return;
+	}
+
+	// Remove all non-digit characters from the phone number. 
+	// First, remove all non number characters. 
+	let nonDigitRegex = /[^0-9]+/g;
+	let filteredString = phoneString.replace(nonDigitRegex, "");
+
+	// If the phone number doesn't have 10 or 14 numbers, alert the user of 
+	// invalid input. 
+	if (filteredString.length < P_NUMBER_LENGTH || 
+		filteredString.length > P_NUMBER_LENGTH + P_CONTRY_CODE_LENGTH) 
+	{
+		alert("Phone Numbers must have at between " + P_NUMBER_LENGTH +
+				 " and " + (P_NUMBER_LENGTH + P_CONTRY_CODE_LENGTH) +
+				  " digits.");
+		return;
+	}
+	
+
 	// Add the infomration to a new row in the table. 
-	addNewRow(firstNameString + lastNameString, emailString, phoneString, 
-				birthdayString, colorString, genderNode.value, activities);
+	addNewRow(firstNameString + " " + lastNameString, emailString, filteredString, 
+				birthdayString, colorString, genderNode.innerText, activities);
 } // end of onSubmitClick
 
 
-// 4.2. Creates a new table row in the table
-//TODO: Add parameter contents 
+// 4.2. Creates a new table row in the table withthe given parameters. 
+// @param {string} name - The full name of the person. 
+// @param {string} email - An email address
+// @param {string} phone - A phone number
+// @param {string} birthday - The date of birth
+// @param {string} favColor - A text version of a color
+// @param {string} gender - The person's gender. 
+// @param {array of string} activites - The activities the person partakes in. 
 function addNewRow(name, email, phone, birthday, favColor, gender, activities) {
+	// Create a new row element, with row scope
+	let newRow = document.createElement("tr");
+	newRow.setAttribute("scope", "row");
 
+	// Append the name to the new row. 
+	appendTableData(newRow, name);
+
+	// Append the email to the new row. 
+	appendTableData(newRow, email);
+	
+	// Append the phone to the new row. 
+	appendTableData(newRow, phone);
+	
+	// Append the birthday to the new row. 
+	appendTableData(newRow, birthday);
+
+	// Append the favorite color to the new row. 
+	appendTableData(newRow, favColor);
+
+	// Append the gender to the new row. 
+	appendTableData(newRow, gender);
+
+	// Append the activities in an unordered list in a new TD element
+	let activitiesTD = document.createElement("td");
+	let activitiesList = document.createElement("ul");
+
+	// Add each activity to the list in the TD. 
+	for (activity of activities) {
+		let activityLI = document.createElement("li");
+		activityLI.innerHTML = activity;
+		activitiesList.appendChild(activityLI);
+	}
+	activitiesTD.appendChild(activitiesList);
+
+	// Add the activities TD to the table row. 
+	newRow.appendChild(activitiesTD);
+
+	// Add the newRow to the body of the table. The table is not specified by 
+	// ID, but there is only one table, so we can search by table tag. 
+	document.getElementsByTagName("table")[0].getElementsByTagName("tbody")[0].appendChild(newRow);
 } // end of addNewRow
 
 
+// 4.3. A helper function that appends the specified string to the specified row
+// inside of a table data element. 
+function appendTableData(row, string) {
+	// Create a new table data element for the string
+	let tableData = document.createElement("td");
+	tableData.innerText = string;
+	// Add it to the row element
+	row.appendChild(tableData);
+} // end of appendTableData
+
+
+// 4.4 A helper function for the validation of emails. 
+// Returns true if the email format is valid. False otherwise. 
+function isValidEmail(email) {
+	// Handle special case when string is empty
+	if (email.length == 0) {
+		return false;
+	}
+	var regexp = /[^@]+@[^@]+/g;
+	//Assert that there is only one '@' symbol, and only allowed characters for the 
+	// local name, and one or more groups of letters surrounding a '.' after the '@'
+	return ((email.match(regexp)||[""])[0].length == email.length) &&
+			(email.match("[!#$%&'*+\-/=?^_`{|}~.a-zA-Z0-9]+@([a-zA-Z]+\\.[a-zA-Z]+)+") != null);
+} // end of isValidEmail
 
 
 
+
+
+
+
+
+
+// 5.1 Opens the details tag for the specified node. 
+function openDetails(node) {
+	node.setAttribute("open", "");
+} // end of openDetails
+
+// 5.2 Closes the details tag
+function closeDetails(node) {
+	node.removeAttribute("open");
+} // end of closeDetails
+
+
+
+
+
+
+// 6.1 
 
 
 setAnchorLinks();
 disablePluto();
 // Add event listeners for when the submit button is clicked. 
 document.getElementById("form-sub").addEventListener("click", alienText);
-document.getElementById("form-sub").addEventListener("click", onSubmitClick);
+
+// Add event listeners for when the mouse goes over a details tag
+detailsTags = document.getElementsByTagName("details");
+for (let i=0; i<detailsTags.length; i++) {
+	detailsTags[i].setAttribute("onmouseenter", "openDetails(this)");
+	detailsTags[i].setAttribute("onmouseleave", "closeDetails(this)");
+}
+
+
 
 
