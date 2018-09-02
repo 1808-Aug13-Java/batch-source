822f32b0f5ccafd6f922855f156c8118d9f83635
Austen <austen.schips@gmail.com>
2018-09-02 19:08:22 -0400

    Dom Project 1

---
+++ Index.js
@@ -0,0 +1,12 @@
+//google, twitter, slack, javadocs
+
+var ele = document.getElementById("google");
+
+ele.onclick = function() {
+    ele.href = "https://www.google.com"; 
+    return false; };
+//document.getElementById("google").href = "https://www.google.com";
+//document.getElementById("google").target = "_blank";
+//
+//element.setAttribute('google','http://www.google.com');
+//document.getElementsByTagName("a")[0].setAttribute("google", "http://www.google.com");
