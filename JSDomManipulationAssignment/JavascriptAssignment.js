deb12a80ba8e746864ea0b10f75b532b78892991
Jan Balangue <jantonio.balangue@gmail.com>
2018-09-02 19:07:11 -0400

    Finished JSDomManipulationAssignment

--- JSDomManipulationAssignment/JavascriptAssignment.js
+++ JSDomManipulationAssignment/JavascriptAssignment.js
@@ -125,7 +125,7 @@ function closeDetails() {
 function printSpans() {
     const spans = document.getElementsByTagName("span");
     let output = "";
-    for (let i = 0; i < spans.length; i++){
+    for (let i = 0; i < spans.length; i++) {
         output += spans[i].innerHTML;
     }
     console.log(output);
@@ -133,3 +133,108 @@ function printSpans() {
 printSpans();
 
 // 7
+document.getElementById("earth_time_check").addEventListener("click", updateEarthTime);
+
+function updateEarthTime() {
+    const today = new Date();
+    const getHour = today.getHours();
+    const getMinutes = today.getMinutes();
+    let getMinutesZero = "";
+    if (getMinutes < 10) {
+        getMinutesZero = "0";
+    }
+    const getSeconds = today.getSeconds();
+    let getSecondsZero = "";
+    if (getSeconds < 10) {
+        getSecondsZero = "0";
+    }
+    document.getElementById("earth_time").innerHTML = getHour + ":" + getMinutesZero + getMinutes + ":" + getSecondsZero + getSeconds;
+}
+
+// 8
+document.getElementById("mars_time_check").addEventListener("click", updateMarsTime);
+
+function updateMarsTime() {
+    const now = Date.now();
+    const earthSeconds = now / 1000;
+    const earthMinutes = earthSeconds / 60;
+    const earthHours = earthMinutes / 24;
+    const earthDays = earthHours / 60;
+    const marsYears = earthDays / 687;
+    document.getElementById("mars_time").innerHTML = "Mars years: " + marsYears;
+}
+
+document.getElementById("acb_time_check").addEventListener("click", searchPlanet);
+
+function searchPlanet() {
+    fetch('http://www.astropical.space/astrodb/api-exo.php?which=name&limit=prox&format=json')
+        .then(function (response) {
+            return response.json().then(function (val) {
+                const proximaCentauriPeriod = val.exoplanets[0].per;
+                const now = Date.now();
+                const earthSeconds = now / 1000;
+                const earthMinutes = earthSeconds / 60;
+                const earthHours = earthMinutes / 24;
+                const earthDays = earthHours / 60;
+                const proximaCentauriYears = earthDays / proximaCentauriPeriod;
+                document.getElementById("acb_time").innerHTML = "Proxima Centauri b years: " + proximaCentauriYears;
+            })
+        })
+}
+
+// 9
+const spanIGHeadings = document.getElementsByTagName("span");
+spanIGHeadings[0].addEventListener("click", changeRandomColor);
+
+function changeRandomColor() {
+    setTimeout(function () {
+        const letters = '123456789ABCDEF';
+        let color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 15)];
+        }
+        document.body.style.background = color;
+    }, 3000);
+}
+
+// 10
+document.getElementById("operation").addEventListener("click", function () {
+    const n1 = document.getElementById("n1").value;
+    const n2 = document.getElementById("n2").value;
+    if (!isNaN(n1) && !isNaN(n2)) {
+        const opts = document.getElementById("operation");
+        selectedOpt = opts.options[opts.selectedIndex].value;
+        console.log(selectedOpt);
+        switch (selectedOpt) {
+            case "Add":
+                document.getElementById("result").innerHTML = parseInt(n1) + parseInt(n2);
+                break;
+            case "Subtract":
+                document.getElementById("result").innerHTML = parseInt(n1) - parseInt(n2);
+                break;
+            case "Multiply":
+                document.getElementById("result").innerHTML = parseInt(n1) * parseInt(n2);
+                break;
+            case "Divide":
+                if (n2 != 0) {
+                    document.getElementById("result").innerHTML = parseInt(n1) / parseInt(n2);
+                } else {
+                    document.getElementById("result").innerHTML = "Cannot divide by 0";
+                }
+                break;
+        }
+    }
+}, { passive: true });
+
+// 11
+function walkTheDOM(node, func) {
+    func(node);
+    node = node.firstChild;
+    while (node) {
+        walkTheDOM(node, func);
+        node = node.nextSibling;
+    }
+}
+walkTheDOM(window.document, function (node) {
+    console.log(node);
+});
