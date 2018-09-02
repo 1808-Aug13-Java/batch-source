d223704e5c712580f0a558882792ddfb55f4d278
Jerry <jbki@protonmail.com>
2018-09-02 19:12:52 -0400

    jsut saving this

--- js/JavascriptAssignment.js
+++ js/JavascriptAssignment.js
@@ -32,45 +32,177 @@ planet.addEventListener("change", function() {
   }
 });
 //When the submit button is pressed, get the values from all of the input into a new row in the table below.  Make sure no input is empty, check that first and last name are at least two letters each. Validate for valid phone number and email structure. This should continue to work for multiple entries and rows.
-var form = $.getElementsByClassName("form-group");
-var sub = $.getElementById("form-sub");
-var firstname = $.getElementById("firstname");
-var lastname = $.getElementById("lastname");
-var email = $.getElementById("email");
-var phone = $.getElementById("phone");
-var bday = $.getElementById("bday");
-var selectedPlanet = planet.selectedIndex;
-var getSelected = function(tag, type, multiple=false) {
-  var radio = $.getElementsByTagName(tag);
-  var selecteds = [];
-  for(r of radio) {
-    if((r.getAttribute("type") === type) && (r.checked === true))
-      selecteds.push(r.value);
-  }
-  if(multiple) 
-    return selecteds;
-  else
-    return selecteds[0];
-}
-var selectedGender = getSelected("input", "radio");
-var selectedActivities = getSelected("input", "checkbox", true); //return array =true
-var table = $.getElementsByTagName("table");
-var newRow = $.createElement("tr");
 
+var sub = $.getElementById("form-sub");
+sub.addEventListener("click", function() {
+  var firstname = $.getElementById("firstname");
+  var lastname = $.getElementById("lastname");
+  var email = $.getElementById("email");
+  var phone = $.getElementById("phone");
+  var bday = $.getElementById("bday");
+  var selectedPlanet = planet.selectedIndex;
+  var getSelected = function(tag, type, multiple=false) {
+    var radio = $.getElementsByTagName(tag);
+    var selecteds = [];
+    for(r of radio) {
+      if((r.getAttribute("type") === type) && (r.checked === true))
+        selecteds.push(r.value);
+    }
+    if(multiple) 
+      return selecteds;
+    else
+      return selecteds[0];
+  }
+  var selectedGender = getSelected("input", "radio");
+  var selectedActivities = getSelected("input", "checkbox", true); //return array =true
+  var selectedColor = $.getElementById("color"); 
+  var table = $.getElementsByTagName("tbody")[0];
+  var createColumn = function(el) {
+    var column = $.createElement("td"); 
+    var createLi = function(txt) {
+      var li = $.createElement("li");
+      var text = $.createTextNode(txt);  
+      li.appendChild(text);
+      return li;
+    }
+    if(Array.isArray(el)) {
+      var ul = $.createElement("ul");
+      for(var i of el) {
+        ul.appendChild(createLi(i));
+      }
+      column.appendChild(ul);
+    }
+    else {
+      var text = $.createTextNode(el);
+      column.appendChild(text);
+    }
+    return column; 
+  }
+  //newColumn.appendChild($.createTextNode(firstname.value));
+  var newRow = $.createElement("tr");
+  newRow.appendChild(createColumn(firstname.value));
+  newRow.appendChild(createColumn(email.value));
+  newRow.appendChild(createColumn(phone.value));
+  newRow.appendChild(createColumn(bday.value));
+  newRow.appendChild(createColumn(selectedColor.value));
+  newRow.appendChild(createColumn(selectedGender));
+  newRow.appendChild(createColumn(selectedActivities));
+  table.appendChild(newRow);
+});
 
 
 //Create a function openDetails() which opens the details element. Invoke this function when the details’ summary is moused over. The details should be hidden when the mouse is removed from the summary.
+var openDetails = function() {
+  this.open = true;
+  details.addEventListener("mouseout", function() {
+    this.open = false;
+  });
+}
+
+var details = $.getElementsByTagName("details")[0];
+details.addEventListener("mouseover", openDetails);
 
 //Create a function that concatenates the inner HTML of all of the span elements and prints the results to the console.
-//
+var concatSpan = function() {
+  var spans = $.getElementsByTagName("span");
+  var res = "";
+  for(var s of spans) {
+    res += s.innerHTML;
+  }
+  console.log(res);
+}
+
 //Create a function that displays the current time on earth in the span with id “earth_time”. Invoke this function when “Earth time” button is clicked. 
-//
+var getEarthTime = function() {
+  var d = new Date();
+  var zeroPad = function(n) {
+    if(n.toString().length === 1) {
+      return "0" + n;
+    }
+    else {
+      return n;
+    }
+  }
+  console.log(zeroPad(d.getHours()) + ":" + zeroPad(d.getMinutes()) + ":" + zeroPad(d.getSeconds()));
+}
+var earthTimeButton = $.getElementById("earth_time_check");
+earthTimeButton.addEventListener("click", getEarthTime);
+
 //Create two other functions which calculates and displays the time passed on Mars and Alpha Centauri Bb if the onset of January 1st, 1970 occured at the same time. Invoke the respective functions when their time buttons are clicked. An orbital period for Mars is 687 Earth days. Using an external api to get the orbital period for Alpha Centauri Bb. (try http://www.astropical.space/astrodb/apiref.php) Provide an implementation for getting this value using both AJAX and the fetch API.
-//
+var getYears = function(ms, period) {
+  var ss = ms/1000;
+  var mm = ss/60;
+  var hh = mm/60;
+  var dd = hh/24;
+  var yy = dd/period;
+  return yy;
+}
+var getMarsTime = function() {
+  var d = new Date();
+  var marsTime = $.getElementById("mars_time");
+  marsTime.innerHTML = getYears(d.getTime(), 687);
+}
+
+var marsTimeCheck = $.getElementById("mars_time_check");
+marsTimeCheck.addEventListener("click", getMarsTime);
+
+
+var acbTimeCheck = $.getElementById("acb_time_check");
+acbTimeCheck.addEventListener("click", function() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if((xhr.readyState === 4) && (xhr.status === 200)) {
+      //console.log(xhr.responseText);
+      var exoplanets = JSON.parse(xhr.responseText).exoplanets;
+      for(var p of exoplanets) {
+        if(p.id === 123) {
+          var d = new Date();
+          var acbTime = $.getElementById("acb_time");
+          acbTime.innerHTML = getYears(d.getTime(), p.per);
+        }
+      }
+    }
+    else {
+      console.log("nope");
+      console.log(xhr.readyState);
+      console.log(xhr.status);
+    }
+  }
+  xhr.open('get', 'http://www.astropical.space/astrodb/api-exo.php?which=name&limit=0&format=json', true);
+  xhr.send();
+});
 //Three seconds after a user clicks on the “Intergalactic Directory” heading, the     background color should change to a random color. Make sure this color is never black so we can still read our black text! (there are other dark colors it could change to where we also couldn’t see the text but it’s enough to just accomodate for a black background)
-//
+var interspans = $.getElementsByTagName("span");
+var getRandomColor = function() {
+    var colorCode = "";
+    colorCode = Math.floor(Math.random()*100+1) + ',';
+    colorCode += Math.floor(Math.random()*100+1) + '%,';
+    colorCode += Math.floor(Math.random()*100+1) + '%,';
+    colorCode += Math.random()*(0.7-0.2) + 0.2;
+
+    return 'hsla(' + colorCode + ')';
+    
+}
+for(var s of interspans) {
+  if(s.innerHTML === "Intergalactic ") {
+    var body = $.getElementsByTagName("body")[0];
+    var style = "background-color:" + getRandomColor();
+    body.setAttribute("style", style);
+    console.log(body);
+  }
+}
 //When inputs with id n1 and n2 have valid numerical input, perform the operation specified in the select. Display the result in the element with id result.
-//
+var doMath = function() {
+  var n1 = $.getElementById("n1");
+  var n2 = $.getElementById("n2");
+  var op = $.getElementById("operation");
+  if(op.value === 'Add') {
+    
+  }
+}
+console.log();
+
+
 //Define function walkTheDom(node, func)
 //    This function should traverse every node in the DOM. 
 //    Use recursion. On each node, calle func(node).
