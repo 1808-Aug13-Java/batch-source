ba3288eb2fd6c60d825ad1aee1e0ee1626e92b5d
David Calkins <david.calkins.1000@gmail.com>
2018-09-04 15:48:14 -0400

    Added hander methods for get and post

--- ServletAssignment/src/main/java/com/revature/servlets/LoginServlet.java
+++ ServletAssignment/src/main/java/com/revature/servlets/LoginServlet.java
@@ -1,7 +1,27 @@
 package com.revature.servlets;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet {
-
-}
+	
+	/** Displays login page. */
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException 
+	{
+		
+	}
+	
+	/** Handles login requests. */
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException 
+	{
+		
+	}
+} // end of class LoginServlet
