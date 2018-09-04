9e7e064ec00aa4f1168d47856d42552a370abaa4
Chandrika Sinha <sinhac@rpi.edu>
2018-09-04 07:56:05 -0400

    DOM assignment

--- .sonarlint/modules/ClientSideTechPractice/issues/index.pb
+++ .sonarlint/modules/ClientSideTechPractice/issues/index.pb
@@ -19,27 +19,17 @@ I
 HTMLCSSPractice/Home.html,3\d\3ded8834934de4b6b95f804e7a269deff0039c6f
 Q
 !JavaScript Assignment/JSPage.html,9\9\99949dd532d0e005189bb838bb13d33e054dad81
-I
-JSPractice/ScopingDemo.js,8\b\8bc78c86fc908b3bb4b9ddf49480bf58340ecd9d
 U
 %JavaScript Assignment/JSAssignment.js,d\f\dfe65e65412a835f830cfb650a69f53a7303ba73
-G
-JSPractice/Student.html,6\3\630f2f99a91793f3f1798709eebd83bcb4c0b722
-K
-JSPractice/StudentScript.js,d\5\d5762e445d0443d3496528455ebd5ece2c1be05e
-D
-JSPractice/Quiz.html,0\5\0562cb3062dc21c1a35b90b454967c8ffa83cb4b
-H
-JSPractice/QuizScript.js,a\b\ab00ac074c5fef214930d0d50a586a29cdd05c02
 Z
 *JSPractice/WeatherApp/WeatherController.js,2\b\2b395de027c828498945c11e870f30f38833f2a5
 R
 "JSPractice/WeatherApp/Weather.html,5\1\51112e75dbbd262133c2310810cbeb98c2fcaa27
 H
-JSPractice/BeerScript.js,7\9\7920dc45dce5cd7ed32b854a96229f70cad26970
-T
-$JSPractice/PairProgramming/Beer.html,8\9\89ae5178199b700052309f373e66ddd51a152933
-Y
-)JSPractice/PairProgramming/EatStreet.html,8\c\8c9f7366a8dd625cea6df9acc4fc216cbc67fc4b
-]
--JSPractice/PairProgramming/EatStreetScript.js,8\4\84e6b810c91ddff0954c3a2472289a8327ddf79b
+DomAssignment/Index.html,c\4\c4b4a6a45cca787438af2bc8a1b9e82423bfc764
+P
+ JSPractice/Other Demos/JSDemo.js,9\9\996f988aa92c137897a8856f4c84ffbdf1d9a3bd
+[
++JSPractice/EventPropDemo/EventPropScript.js,b\a\ba44e6aceb63e5133930d048cf495d6aeec89209
+U
+%DomAssignment/JavascriptAssignment.js,7\d\7d478f0200c7c727b89b85e21efcef7fdbc43a13
---
+++ ClientSideTechPractice/DomAssignment/JavascriptAssignment.js
@@ -0,0 +1,179 @@
+/**
+ * Chandrika Sinha
+ */
+
+// QUESTION 1
+function question1() {
+	document.getElementsByName("google")[0].innerText = "Google";
+	document.getElementsByName("google")[0].setAttribute("href", "https://www.google.com/");
+	
+	document.getElementsByName("twitter")[0].innerText = "Twitter";
+	document.getElementsByName("twitter")[0].setAttribute("href", "https://twitter.com/")
+
+	document.getElementsByName("slack")[0].innerText = "Slack";
+	document.getElementsByName("slack")[0].setAttribute("href", "https://slack.com/");
+
+	document.getElementsByName("javadocs")[0].innerText = "Javadocs";
+	document.getElementsByName("javadocs")[0].setAttribute("href", "https://javadocs.com/");
+}
+question1();
+
+// QUESTION 2
+function question2() {
+	document.getElementsByTagName("option")[3].setAttribute("disabled", "true");
+}
+question2();
+
+// QUESTION 3
+function alienText() {
+	let elements = document.getElementsByTagName("p");
+	for (var i = 0; i < elements.length; i++) {
+		elements[i].hidden = false;
+	}
+}
+function question3() {
+	// When any planet other than Earth is selected, execute this function
+	alienText();
+}
+question3();
+
+// QUESTION 4
+document.getElementById("form-sub").addEventListener("click", showValues);
+function showValues() {
+	let first = document.getElementById("firstname");
+	let last = document.getElementById("lastname");
+	let email = document.getElementById("email");
+	let phone = document.getElementById("phone");
+	let bday = document.getElementById("bday");
+	let gender = document.querySelector('input[name = "gender"]:checked');
+	let color = document.getElementById("color");
+	let activities = document.querySelectorAll('input[type = "checkbox"]:checked');
+
+	addRow(first, last, email, phone, bday, gender, color, activities);
+}
+
+function addRow(first, last, email, phone, birthday, gender, color, activities) {
+	console.log(first);
+	let row = document.createElement("tr");
+	let cell1 = document.createElement("td");
+	let cell2 = document.createElement("td");
+	let cell3 = document.createElement("td");
+	let cell4 = document.createElement("td");
+	let cell5 = document.createElement("td");
+	let cell6 = document.createElement("td");
+	let cell7 = document.createElement("td");
+	
+	cell1.innerText = first+' '+last;
+	cell2.innerText = email;
+	cell3.innerText = phone;
+	cell4.innerText = birthday;
+	cell5.innerText = color;
+	cell6.innerText = gender;
+	
+	let act = document.createElement("ul");
+	for (var i = 0; i < activities.length; i++) {
+		let activity = document.createElement("li");
+		activity.innerHTML = activities[i];
+		act.appendChild(activity);
+	}
+
+	row.appendChild(cell1);
+	row.appendChild(cell2);
+	row.appendChild(cell3);
+	row.appendChild(cell4);
+	row.appendChild(cell5);
+	row.appendChild(cell6);
+	row.appendChild(cell7);
+	cell7.appendChild(act);
+	
+	document.getElementsByClassName("table table-striped table-bordered table-hover table-sm")[0].appendChild(row);
+}
+
+// QUESTION 5
+function openDetails() {
+	this.open = true;
+}
+function closeDetails() {
+	this.open = false;
+}
+document.getElementsByTagName("details")[0].addEventListener("mouseenter", openDetails);
+document.getElementsByTagName("details")[0].addEventListener("mouseleave", closeDetails);
+
+// QUESTION 6
+function question6() {
+	let spans = document.getElementsByTagName("span");
+	let concatentations = '';
+	for (var i = 0; i < spans.length; i++) {
+		concatentations+=spans[i].innerHTML;
+	}
+	console.log(concatentations);
+}
+question6();
+
+// QUESTION 7
+function question7() {
+	var d = new Date();
+	document.getElementById("earth_time").innerHTML = d.getHours() + ':' + d.getMinutes();
+}
+document.getElementById("earth_time_check").addEventListener("click", question7);
+
+// QUESTION 8
+function timePassedOnMars() {
+	var x = new Date();
+	let earthYearsPassed = x.getFullYear() - 1970;
+	let earthDays = (earthYearsPassed*365.25) + x.getDay();
+	let marsYears = earthDays / 687;
+	let marsDays = earthDays % 687;
+	document.getElementById("mars_time").innerText = Math.floor(marsYears) + " years and " + marsDays + " days";
+}
+document.getElementById("mars_time_check").addEventListener("click", timePassedOnMars);
+
+let baseUrl = "http://www.astropical.space/astrodb/api.php?table=nearby&which=distance&limit=10&format=json";
+function timePassedOnACB() {
+	sendAjaxGet(baseUrl, displayACBTime);
+}
+
+function displayACBTime(xhr) {
+	var stars = xhr.responseText.replace(/\"hd\": ,/g, "");
+	stars = stars.replace(/\"sao\": ,/g, "");
+	stars = stars.replace(/\"hip\": ,/g, "");
+	stars = stars.replace(/\"rad\": ,/g, "");
+	stars = stars.replace(/\"hd\": ,/g, "");
+	
+	
+	let info = JSON.parse(stars);
+	
+	console.log(info.hipstars[2]);
+	// let acbDaysPerYear = 0; `Weather for ${weather.location.name}, ${weather.location.region}`
+	let x = new Date();
+	let earthYearsPassed = x.getFullYear() - 1970;
+	let earthDays = (earthYearsPassed*365.25) + x.getDay();
+	let acbYears = earthDays / 687;
+	let acbDays = earthDays % 687;
+	document.getElementById("acb_time").innerText = Math.floor(acbYears) + " years and " + acbDays + " days";
+}
+
+function sendAjaxGet(url, callback) {
+	let xhr = (new XMLHttpRequest() || new ActiveXObject("Mircrosoft.HTTPRequest"));
+	xhr.onreadystatechange = function() {
+		if (this.readyState === 4 && this.status) {
+			callback(this);
+		}
+	}
+	xhr.open("GET", url);
+	xhr.send();	
+}
+
+document.getElementById("acb_time_check").addEventListener("click", timePassedOnACB);
+
+// QUESTION 9
+function question9() {
+	this.style.backgroundColor = "red";
+}
+// window.setTimeout(question9, 3000);
+document.getElementsByTagName("h1")[0].addEventListener("click", question9);
+
+// QUESTION 10
+
+
+// QUESTION 11
