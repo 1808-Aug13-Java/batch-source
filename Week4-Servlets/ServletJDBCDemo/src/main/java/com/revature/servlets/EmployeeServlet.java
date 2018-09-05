a8c12d21a4b4fad8c3ea6cdda19b74ef9315af5c
Carolyn Rehm <carolynhrehm@gmail.com>
2018-09-05 10:26:04 -0400

    Week 4 - Wednesday morning demo

---
+++ Week4-Servlets/ServletJDBCDemo/src/main/java/com/revature/servlets/EmployeeServlet.java
@@ -0,0 +1,54 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.dao.EmployeeDao;
+import com.revature.dao.EmployeeDaoImpl;
+import com.revature.model.Employee;
+
+/**
+ * Servlet implementation class EmployeeServlet
+ */
+public class EmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EmployeeDao ed = new EmployeeDaoImpl();
+		List<Employee> employees = ed.getEmployees();
+		
+		ObjectMapper om = new ObjectMapper();
+		String employeeString = om.writeValueAsString(employees);
+		employeeString = "{\"employees\":"+employeeString+"}";
+		
+		PrintWriter pw = response.getWriter();
+		pw.print(employeeString);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Week4-Servlets/ServletJDBCDemo/src/main/java/com/revature/util/ConnectionUtil.java
@@ -0,0 +1,46 @@
+package com.revature.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+	
+	private static Connection connection;
+
+	public static Connection getHardCodedConnection() throws SQLException {
+		String url = "jdbc:oracle:thin:@[url]:1521:ORCL";
+		String username = "Admin";
+		String password = "p4ssw0rd";
+		if(connection == null || connection.isClosed()) {
+			connection = DriverManager.getConnection(url, username, password);
+		}
+		return connection;
+	}
+	
+	public static Connection getConnection() throws IOException, SQLException {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		Properties prop = new Properties();
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		prop.load(loader.getResourceAsStream("connection.properties"));
+		String url = prop.getProperty("url");
+		String username = prop.getProperty("username");
+		String password = prop.getProperty("password");
+		if(connection == null || connection.isClosed()) {
+			connection = DriverManager.getConnection(url, username, password);
+		}
+		return connection;
+	}
+	
+}
