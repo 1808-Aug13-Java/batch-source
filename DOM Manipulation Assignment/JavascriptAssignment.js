322ffcaa6ef85c47c2de5ef88b9af6d8584a8a47
jgonzd <jonathan.gonzalezdavila@lc.cuny.edu>
2018-09-04 22:24:40 -0400

    Updated DOM assignment

--- DOM Manipulation Assignment/JavascriptAssignment.js
+++ DOM Manipulation Assignment/JavascriptAssignment.js
@@ -1,8 +1,11 @@
 function alienText(){ 
+
+    var b = document.createAttribute("hidden");
+
     if(document.getElementById("planet").value !="Earth"){
         document.getElementsByTagName("p")[5].removeAttribute("hidden");
     } else if(document.getElementById("planet").value =="Earth"){
-        document.getElementsByTagName("p")[5].addAttribute("hidden");
+        document.getElementsByTagName("p")[5].setAttributeNode(b);
     }
 }
 
@@ -75,6 +78,9 @@ function addNew(){
     console.log(activity);
 
 
+    var regex1 = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    var regex2 = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
     // console.log(`name is ${name} and major is ${major}`);
 
     // if((name && major) != ""){
@@ -83,7 +89,7 @@ function addNew(){
     //     document.createElement("p");
     // }
 
-    if((firstname && lastname && email && phone && bday && gender && color && activity)){
+    if((firstname && lastname && email && phone && bday && gender && color && activity && regex1.test(phone) && regex2.test(email))){
         if(errorDisplayed){
             removeError();
             errorDisplayed = false;
@@ -103,7 +109,7 @@ function displayError(){
     errorNode.innerHTML = "Invalid Input"
     errorNode.setAttribute("id", "error");
     errorNode.style = "color : red; display : inline; margin-left: 20px;";
-    document.getElementsByTagName("div")[0].appendChild(errorNode);
+    document.getElementsByTagName("div")[3].appendChild(errorNode);
 }
 
 function removeError(){
@@ -113,8 +119,136 @@ function removeError(){
     }
 }
 
+var a = document.createAttribute("open");
+
+function openDetails(){
+
+    document.getElementsByTagName("details")[0].setAttributeNode(a);
+
+}
+
+function closeDetails(){
+
+    document.getElementsByTagName("details")[0].removeAttributeNode(a);
+
+}
 
 let errorDisplayed = false;
 
+//document.getElementsByTagName("p")[3].addAttribute("hidden")
 
 document.getElementById("form-sub").addEventListener("click", addNew);
+
+document.getElementsByTagName("summary")[0].addEventListener("mouseenter", openDetails);
+document.getElementsByTagName("summary")[0].addEventListener("mouseout", closeDetails);
+
+function concat(){
+    spanElements = document.getElementsByTagName("span");
+    text = ""
+
+    for(elements of spanElements){
+
+        text = text + elements.innerHTML;
+
+    }
+
+    console.log(text);
+
+}
+
+function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  
+  function earthTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('earth_time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+      earthTime()
+    }, 500);
+  }
+
+  document.getElementById("earth_time_check").addEventListener("click", earthTime);
+
+
+
+function marsTime(){
+
+    var d = new Date();
+    document.getElementById("mars_time").innerHTML = (d.getTime())/(1.027491252*1000*60*60*24);
+    var t = setTimeout(function() {
+        marsTime()
+      }, 500); 
+
+}
+
+function random_bg_color() {
+
+    event.stopPropagation();   
+
+    var x = Math.floor(Math.random() * 256 + .01);
+    var y = Math.floor(Math.random() * 256 + .01);
+    var z = Math.floor(Math.random() * 256 + .01);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  
+    setTimeout(function(){document.body.style.background = bgColor; }, 3000);
+    
+    
+    
+    }
+
+document.getElementsByTagName("h1")[0].addEventListener("click", 
+    function(event){random_bg_color();}
+);
+
+
+
+document.getElementById("mars_time_check").addEventListener("click", marsTime);
+
+function calculator(){
+    var operation = document.getElementById("operation").value;
+    var result;
+    n1 = parseFloat(document.getElementById("n1").value);
+    n2 = parseFloat(document.getElementById("n2").value);
+
+    switch(operation){
+        case "Add":
+            result = n1+n2;
+            break;
+        case "Multiply":
+            result = n1*n2;
+            break;
+        case "Divide":
+            result = n1/n2;
+            break;
+        case "Subtract":
+            result = n1-n2;
+            break;
+    }
+
+    document.getElementById("result").innerHTML = result;
+
+}
+
+document.getElementById("calc-sub").addEventListener("click", calculator);
+
+function walkTheDom(node, func) {
+    var children = node.childNodes;
+    for (var i = 0; i < children.length; i++)  // Children are siblings to each other
+        walkTheDom(children[i], func);
+    func(node);
+ }
+
+function func(x){
+    console.log(x);
+}
+
+//walkTheDom(document, func);
