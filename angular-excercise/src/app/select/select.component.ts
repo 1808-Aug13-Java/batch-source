1424d0bfd7f50210b325b32148c098f2db896999
Austen <austen.schips@gmail.com>
2018-09-08 18:30:35 -0400

    second push

--- angular-excercise/src/app/select/select.component.ts
+++ angular-excercise/src/app/select/select.component.ts
@@ -6,35 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select.component.css']
 })
 export class SelectComponent implements OnInit {
-  entries = [];
-  selectedEntry: { [key: string]: any } = {
-    value: null,
-    description: null
-  };
   constructor() { }
-
+Something(){
+  return 0;
+}
+  animalArr = ["dog", "cat", "duck"];
+  colorArr = ["black", "blue", "death"];
+  dayArr = ["Fat-Tuesday", "Wide-Wednesday"];
   ngOnInit() {
-    this.entries = [
-      {
-        description: 'animals',
-        id: 1
-      },
-      {
-        description: 'colors',
-        id: 2
-      },
-      {
-        description: 'days',
-        id: 3
-      }];
-    // onSelectionChange(entry) {
-    //   // clone the object for immutability
-    //   this.selectedEntry = Object.assign({}, this.selectedEntry, entry);
-    // }
-    // if(this.entries) {
-    //   this.onSelectionChange(this.entries[0]);  
-    // }
     
   }
-
+ 
 }
