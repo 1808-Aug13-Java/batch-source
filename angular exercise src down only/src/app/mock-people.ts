7b7ad8f37fed5bcaccfbd33e920387c0384e3909
Nozuko <nozukosutherland@gmail.com>
2018-09-10 02:00:37 -0400

    angular exercise re-upload due to push issues
    re-upload because too many files before (?) + change to table names

---
+++ angular exercise src down only/src/app/mock-people.ts
@@ -0,0 +1,19 @@
+import { Person } from "./person";
+
+export const PEOPLE: Person[] = [{
+    firstname: 'William', 
+    lastname: 'Darcy', 
+    email: 'willd@gmail.com', 
+    birthday: new Date('Aug 01 1900') 
+}, {
+    firstname: 'Corey', 
+    lastname: 'Money', 
+    email: 'getMoney@getmoney.com', 
+    birthday: new Date('Sep 08 2000') 
+}, {
+    firstname: 'Amy', 
+    lastname: 'Pond', 
+    email: 'iluvdoc@who.com', 
+    birthday: new Date('Oct 20 2020')
+}
+];
