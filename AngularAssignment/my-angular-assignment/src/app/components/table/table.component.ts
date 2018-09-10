a55eb3e21adb53262249b6ef3fe6b13b9df05240
AHuertaCS <ahuertacs@gmail.com>
2018-09-10 16:36:20 -0400

    AngularAssignment

--- AngularAssignment/my-angular-assignment/src/app/components/table/table.component.ts
+++ AngularAssignment/my-angular-assignment/src/app/components/table/table.component.ts
@@ -13,15 +13,11 @@ export class TableComponent implements OnInit {
   }
   
 
-  people = [{ first: "crin", last: "toghill", email: "ctoghill0@mysql.com", birthday: "3/28/1993" },{
-     first: "berte", last: "sprigings", email: "bsprigings1@dailymotion.com", birthday: "8/9/1996" },{
-     first: "emmanuel", last: "duffit", email: "eduffit2@ask.com", birthday: "10/25/1998" },{
-     first: "lilah", last: "yeo", email: "lyeo3@amazon.co.uk", birthday: "3/8/1993" },{
-     first: "whit", last: "chowne", email: "wchowne4@1und1.de", birthday: "6/13/1996" },{
-     first: "lin", last: "cutsforth", email: "lcutsforth5@nyu.edu", birthday: "8/4/1992" },{
-     first: "vinni", last: "vevers", email: "vvevers6@mail.ru", birthday: "12/29/1990" },{
-     first: "dominica", last: "earley", email: "dearley7@fc2.com", birthday: "7/20/1997" },{
-     first: "ludovico", last: "rikel", email: "lrikel8@oaic.gov.au", birthday: "12/31/1996" },{
-     first: "marshal", last: "copner", email: "mcopner9@ifeng.com", birthday: "3/21/1992" }]
+  people = [{ first: "john", last: "patterson", email: "jpatt213@gmail.com", birthday: "5/12/1981" },{
+     first: "ban", last: "leo", email: "banleo0912@yahoo.com", birthday: "8/10/1998" },{
+     first: "jaina", last: "proudmoore", email: "jainaproud12@gmail.com", birthday: "11/09/1996" },{
+     first: "cjay", last: "pj", email: "cjayride@outlook.com", birthday: "3/8/1993" },{
+     first: "lily", last: "huerta", email: "lilyhuerta101@gmail.com", birthday: "9/23/2096" },{
+     first: "cooper", last: "gege", email: "cooperg@yahoo.com", birthday: "10/11/1990" }]
 
 }
