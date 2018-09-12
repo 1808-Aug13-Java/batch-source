155abbbb150824e617fb23e8d7d2a0048692bd4f
David Calkins <david.calkins.1000@gmail.com>
2018-09-12 00:35:48 -0400

    View Pending

--- p1-david-calkins/src/main/java/com/revature/servlets/helpers/ResourceRequestHelper.java
+++ p1-david-calkins/src/main/java/com/revature/servlets/helpers/ResourceRequestHelper.java
@@ -1,10 +1,24 @@
 package com.revature.servlets.helpers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.dao.EmployeeDao;
+import com.revature.dao.EmployeeDaoImpl;
+import com.revature.dao.ManagerDao;
+import com.revature.dao.ManagerDaoImpl;
+import com.revature.dao.ReimbursementDao;
+import com.revature.dao.ReimbursementDaoImpl;
+import com.revature.models.Employee;
+import com.revature.models.Reimbursement;
+import com.revature.util.DBConnectionUtil;
 import com.revature.util.LogUtil;
 
 public class ResourceRequestHelper {
@@ -17,6 +31,7 @@ public class ResourceRequestHelper {
 	
 	
 	private static final String LOGOUT = "logout";
+	private static final String PENDING = "pending";
 	
 	public static boolean isResourceRequest(HttpServletRequest request) {
 		return request.getParameter(RESOURCE_REQUEST) != null;
@@ -34,6 +49,20 @@ public class ResourceRequestHelper {
 			return;
 		}
 		
+		// Get Appropriate Dao's 
+		EmployeeDao empDao = new EmployeeDaoImpl();
+		ManagerDao manDao = new ManagerDaoImpl();
+		ReimbursementDao remDao = new ReimbursementDaoImpl();
+		
+		// Get the username from the request
+		String username = (String) request.getSession(false).getAttribute("username");
+		
+		// Get the print writer to the client response
+		PrintWriter pw = response.getWriter();
+		
+		// Used to map json
+		ObjectMapper om = null;
+		
 		// Get the string that represents the resource the user is requesting
 		String resourceReq = request.getParameter(RESOURCE_REQUEST) ;
 		LogUtil.logInfo("Requested Resource: " + resourceReq);
@@ -43,6 +72,30 @@ public class ResourceRequestHelper {
 			System.out.println("Logged Out ");
 			request.getSession(false).invalidate();
 			return;
+		}
+		
+		// Get a connection. We're going to need it for pretty much all requests
+		try (Connection con = DBConnectionUtil.getConnection()) {
+			// Initialize object mapper. 
+			om = new ObjectMapper();
+			
+			// If the user is requesting pending reimbursements, fetch, and return 
+			// as JSON. 
+			if (resourceReq.equals(PENDING)) {
+				Employee emp = empDao.getEmployeeByUsername(username, con);
+				
+				// Get the pending requests for the specified employee
+				List<Reimbursement> reimbursements = remDao.getPendingByRequester(emp, con);
+				
+				String remString = om.writeValueAsString(reimbursements);
+				
+				// Send the output and return. 
+				pw.println(remString);
+				return;
+			}
+		} catch (SQLException ex) {
+			// If sql error, send a 500 code, and return. 
+			response.sendError(500);
 		}
 	} // end of routeResource
 	
--- p1-david-calkins/src/main/webapp/Scripts/mainPage.js
+++ p1-david-calkins/src/main/webapp/Scripts/mainPage.js
@@ -7,6 +7,13 @@ console.log("Main Page Script Loaded");
 const URL = "http://localhost:8080/p1-david-calkins/reimbursement";
 
 const REQUEST = "resourceRequest"
+	
+var lastXHR = undefined;
+
+
+// ============================================================
+// Request Functions 
+// ============================================================
 
 // Attempts to logout, sending client to login page. 
 function logout() {
@@ -15,6 +22,107 @@ function logout() {
 		console.log("Logged Out");
 		window.location.reload(true);
 	});
+}
+
+// Attempts to get pending reimbursements
+function getPending() {
+	console.log("Requested Pending");
+	sendAjaxGet(URL + "?" + REQUEST + "=" + "pending", function (xhr) {
+		
+		// Display the reimbursements
+		let reimbursements = JSON.parse(xhr.responseText);
+		console.log(reimbursements);
+		displayReimbursements(reimbursements);
+	});
+	return false;
+}
+
+
+
+// ============================================================
+// Display Functions 
+// ============================================================
+
+function displayReimbursements(reimbursements) {
+	// Create the layout of the table
+	let table = document.createElement("table");
+	let thead = document.createElement("thead");
+	let tbody = document.createElement("tbody");
+	
+	// Create 9 columns
+	let trHead = document.createElement("tr");
+	
+	// Creates a new column and ads it to the trHead
+	function addColumn (newColumnText) {
+		let th = document.createElement("th");
+		th.innerHTML = newColumnText;
+		trHead.appendChild(th);
+	}
+	
+	addColumn("Requster Name");
+	addColumn("Requster Username");
+	addColumn("Status");
+	addColumn("Amount");
+	addColumn("Submit Date");
+	addColumn("Description");
+	addColumn("Resolved By");
+	addColumn("Resolve Date");
+	addColumn("Reason");
+	
+	// Add them to the table head. 
+	thead.appendChild(trHead);
+	table.appendChild(thead);
+	
+	
+	
+	// A function to facilitate adding elements to a row
+	function addToRow(tableRow, newData) {
+		let td = document.createElement("td");
+		td.innerHTML = newData;
+		tableRow.appendChild(td);
+	}
+	
+	
+	// For each reimbursement, create a row and add it to the table
+	for (let i=0; i < reimbursements.length; i++) {
+		let trBody = document.createElement("tr");
+		
+		addToRow(trBody, reimbursements[i]["requester"]["name"]);
+		addToRow(trBody, reimbursements[i]["requester"]["username"]);
+		
+		// Get the status
+		let status = reimbursements[i]["status"];
+		addToRow(trBody, reimbursements[i]["status"]);
+		addToRow(trBody, reimbursements[i]["amount"]);
+		addToRow(trBody, reimbursements[i]["submitDate"]);
+		addToRow(trBody, reimbursements[i]["description"]);
+		
+		// If the request is pending, none of the other fields are applicable
+		if (status === "Pending") {
+			for (let i=0; i<3; i++) {
+				addToRow(trBody, "N/A");
+			}
+		}
+		// Otherwise, add he applicable table rows. 
+		else {
+			
+			addToRow(trBody, reimbursements[i]["resolvedBy"]["name"]);
+			addToRow(trBody, reimbursements[i]["resolveDate"]);
+			addToRow(trBody, reimbursements[i]["reason"]);
+		}
+		
+		tbody.appendChild(trBody);
+	}
+	
+	
+	// Add the elements to the table
+	table.appendChild(tbody);
+	
+	// Remove the old output. 
+	document.getElementById("outputContainer").innerHTML = "";
+	
+	// Add the table to the output. 
+	document.getElementById("outputContainer").appendChild(table);
 }
 
 
@@ -36,7 +144,17 @@ function sendAjaxGet(url, callback) {
 		}
 	}
 	
+	// Cancel any previous pending request
+	if (lastXHR != undefined) {
+		lastXHR.abort();
+	}
+	
 	// Open and send the request. 
 	xhr.open("GET", url);
 	xhr.send();
+	
+	// Set the last Request that we made. 
+	LastXHR = xhr;
 }
+
+
