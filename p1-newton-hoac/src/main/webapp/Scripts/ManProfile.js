b793e055fd3264758aad2119221505e07232c1f7
Newton Hoac <nhoac001@ucr.edu>
2018-09-12 09:36:11 -0400

    Finished search by employee id >:3

--- p1-newton-hoac/src/main/webapp/Scripts/ManProfile.js
+++ p1-newton-hoac/src/main/webapp/Scripts/ManProfile.js
@@ -26,6 +26,11 @@ function viewEmployees(){
 	sendAjaxGet("http://localhost:8082/p1-newton-hoac/profile", displayProfiles);
 }
 
+function searchEmployee(){
+	let id = document.getElementById("empId").value;
+	sendAjaxGet("http://localhost:8082/p1-newton-hoac/reimbursement?id=" + id, displayAllReimbursements);
+}
+
 // Display all pending reimbursements
 function displayAllPending(xhr){
 	reimbs = JSON.parse(xhr.responseText);	
@@ -113,7 +118,7 @@ function displayAllResolved(xhr){
 		<td>${descript}</td>
 		<td>${action}</td>`;
 		
-		// Append if pending
+		// Append if resolved
 		if(status == "RESOLVED"){
 			table.appendChild(newRow);
 		}
@@ -154,5 +159,50 @@ function displayProfiles(xhr){
 		<td>${email}</td>`;
 		
 		table.appendChild(newRow);
+	}
+}
+
+function displayAllReimbursements(xhr){
+	reimbs = JSON.parse(xhr.responseText);	
+	let table = document.getElementById("reimbTable");
+	reimbArr = reimbs.reimbursement;
+	
+	while(table.childElementCount > 1){
+		table.removeChild(table.lastChild);
+	}
+	
+	for(i in reimbArr){
+		let newRow = document.createElement("tr");
+		
+		id = `${reimbArr[i].rid}`;
+		
+		eid = `${reimbArr[i].empId}`;
+		
+		if(reimbArr[i].manId){
+			mid = `${reimbArr[i].manId}`;
+		} else {
+			mid = "n/a";
+		}
+		
+		status = `${reimbArr[i].status}`;
+		
+		descript = `${reimbArr[i].description}`;
+		
+		if(reimbArr[i].action){
+			action = `${reimbArr[i].action}`;
+		} else {
+			action = "n/a";
+		}
+		
+		newRow.innerHTML = `<td>${id}</td>
+		<td>${eid}</td>
+		<td>${mid}</td>
+		<td>${status}</td>
+		<td>${descript}</td>
+		<td>${action}</td>`;
+		
+		
+		table.appendChild(newRow);
+		
 	}
 }
