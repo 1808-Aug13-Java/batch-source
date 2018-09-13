0b2d55728dd890a0590ae8c2746156c4635729c7
carolynrehm <32271621+carolynrehm@users.noreply.github.com>
2018-09-12 21:07:02 -0400

    Wednesady Session demo

--- Week4-Servlets/ServletLoginDemo/src/main/java/com/revature/servlets/LoginServlet.java
+++ Week4-Servlets/ServletLoginDemo/src/main/java/com/revature/servlets/LoginServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 /**
  * Servlet implementation class LoginServlet
  */
