d35ceb90065d109ee2ec061b256efc56b6123c7f
carolynrehm <32271621+carolynrehm@users.noreply.github.com>
2018-09-12 21:06:37 -0400

    Wednesady Session demo

--- Week4-Servlets/ServletLoginDemo/src/main/java/com/revature/servlets/ProfileServlet.java
+++ Week4-Servlets/ServletLoginDemo/src/main/java/com/revature/servlets/ProfileServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 /**
  * Servlet implementation class ProfileServlet
  */
