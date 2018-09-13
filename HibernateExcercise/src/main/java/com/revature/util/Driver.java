ebe29769a8a7e48e2b79f29c5502eaa3a530e658
Austen <austen.schips@gmail.com>
2018-09-13 15:11:21 -0400

    Hibernate Thursday Excercise

---
+++ HibernateExcercise/src/main/java/com/revature/util/Driver.java
@@ -0,0 +1,10 @@
+package com.revature.util;
+
+import org.hibernate.Session;
+
+public class Driver {
+	public static void main(String[] args){
+		Session s = HibernateUtil.getSession();
+		s.close();
+	}
+}
