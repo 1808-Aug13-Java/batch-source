3dfded761ade119ed4f892244c123a078fe043f2
Carolyn Rehm <carolynhrehm@gmail.com>
2018-09-20 10:25:44 -0400

    Thursday morning demos

---
+++ Week6-Spring/SpringORMDemo/src/main/java/com/revature/daos/BearDaoImpl.java
@@ -0,0 +1,76 @@
+package com.revature.daos;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.revature.models.Bear;
+
+@Repository
+public class BearDaoImpl implements BearDao {
+	
+	@Autowired
+	private SessionFactory sf;
+
+	@Override
+	@Transactional(propagation=Propagation.MANDATORY)
+	public List<Bear> getBears() {
+		Session s = sf.getCurrentSession();
+		List<Bear> bears = s.createQuery("from Bear").list();
+		return bears;
+	}
+
+	@Override
+	public Bear getBearById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int createBear(Bear b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Bear> getBearsByCave(int caveId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bear> getBearsByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bear> getSBears() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getBearCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Bear> get90andGBears() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bear> get90orGBears() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
