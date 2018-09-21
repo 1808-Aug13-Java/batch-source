426b4e0b7fe9995c44d5d9d3a0c20249d6b8c3d9
Jeremiah LaForge <laforge.jeremiah.s@gmail.com>
2018-09-21 15:51:48 -0400

    final commit

--- RestClient/src/main/java/com/revature/main/Driver.java
+++ RestClient/src/main/java/com/revature/main/Driver.java
@@ -13,7 +13,7 @@ public class Driver {
 	
 	public static void main(String[] args) {
 	
-		String getRequestUrl = "http://192.168.60.170:8084/intern/1";
+		String getRequestUrl = "http://192.168.60.170:8084/interns/101";
 		
 		//RestTemplate is an object provided by Spring Web which allows us to map resources and send Http Requests
 		RestTemplate restTemplate = new RestTemplate();
@@ -26,10 +26,11 @@ public class Driver {
 			log.error("resource consumption unsuccessful");
 		}
 		
-		String postRequestUrl = "http://192.168.60.170:8084/intern";
-		Intern newIntern = new Intern(97L,"Maureen","Johnsan","maureenj@nvcc.edu", "jmaureen","ughwahtever", 100L);
-		Intern newIntern1 = new Intern(98L,"Chad","Bowinger","chad@nvcc.edu", "chadRules","huntokar", 1L);
-		Intern newIntern2 = new Intern(99L,"Dana","Cardinal","themayor@nv.gov", "realDana","jasika", 95L);
+		String postRequestUrl = "http://192.168.60.170:8084/interns/";
+		String deletRequestUrl = "http://192.168.60.170:8084/interns/101";
+		Intern newIntern = new Intern(100L,"Maureen","Johnsan","maureenj@nvcc.edu", "jmaureen","ughwahtever", 100L);
+		Intern newIntern1 = new Intern(101L,"Chad","Bowinger","chad@nvcc.edu", "chadRules","huntokar", 1L);
+		Intern newIntern2 = new Intern(103L,"Dana","Cardinal","themayor@nv.gov", "realDana","jasika", 95L);
 		
 		try {
 			Intern addedIntern = restTemplate.postForObject(postRequestUrl, newIntern, Intern.class);
@@ -41,9 +42,18 @@ public class Driver {
 			Intern addedIntern2 = restTemplate.postForObject(postRequestUrl, newIntern2, Intern.class);
 			log.info("Resource exchange successful");
 			log.info("'Posted' : "+addedIntern2.toString());
+			
 		} catch (Exception e) {
 			log.error("Resource exchange unsuccessful");
 		}
+		try {
+			restTemplate.delete(deletRequestUrl);
+//			restTemplate.delete(postRequestUrl);
+			log.info("Resource delete successful");
+		} catch (Exception e) {
+			log.error("Resource delete unsuccessful :" + e.getMessage() );
+		}
+		
 		
 
 	}
