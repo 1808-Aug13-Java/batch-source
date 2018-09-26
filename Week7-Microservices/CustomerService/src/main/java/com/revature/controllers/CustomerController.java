913ef2df846026d7005abe4ab51de8652d6b70d0
Carolyn Rehm <carolynhrehm@gmail.com>
2018-09-26 11:56:49 -0400

    Wednesday morning demos

--- Week7-Microservices/CustomerService/src/main/java/com/revature/controllers/CustomerController.java
+++ Week7-Microservices/CustomerService/src/main/java/com/revature/controllers/CustomerController.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.intercom.AccountClient;
+import com.revature.models.Account;
 import com.revature.models.Customer;
 
 @RestController
@@ -18,6 +21,9 @@ public class CustomerController {
 	private Logger logger = Logger.getLogger(CustomerController.class.getName());
 	
 	private List<Customer> customers = new ArrayList<>();
+	
+	@Autowired
+	AccountClient accountClient;
 	
 	public CustomerController() {
 		customers.add(new Customer(1, "Billy Pilgrim", "billp@gmail.com", null));
@@ -36,6 +42,9 @@ public class CustomerController {
 	public Customer getCustomerById(@PathVariable("id") int customerId) {
 		logger.info("CustomerController.getCustomerById");
 		Customer customer = customers.stream().filter(cust -> cust.getCustomerId() == customerId).findFirst().get();
+		//we want to make a call to our account service in order to get the accounts associated with our customer
+		List<Account> accounts = accountClient.getAccounts(customerId);
+		customer.setAccounts(accounts);
 		return customer;
 	}
 
---
+++ Week7-Microservices/CustomerService/src/main/java/com/revature/intercom/AccountClient.java
@@ -0,0 +1,17 @@
+package com.revature.intercom;
+
+import java.util.List;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.revature.models.Account;
+
+@FeignClient(name="account-service")
+public interface AccountClient {
+	
+	@GetMapping("accounts/customers/{customerId}")
+	List<Account> getAccounts(@PathVariable("customerId") int customerId);
+
+}
